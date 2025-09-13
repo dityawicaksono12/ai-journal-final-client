58f71cb82f1c7f693fb587d2a0e82d6e41411e72
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-13 18:36:43 -0400

    Scaffold Context setup

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({ children }) => {
+
+    const value = {}
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
