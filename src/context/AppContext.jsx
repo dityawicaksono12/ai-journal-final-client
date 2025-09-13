8acc95d8c0320ed7ea409122b51763a725b30797
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-13 19:42:21 -0400

    Add navigation + state

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,8 +1,15 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext()
 
 export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate()
+    const [user, setUser ] = useState(null);
+    const [chats, setChats ] = useState([]);
+    const [selectedChat, setSelectedChat ] = useState([]);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
     const value = {}
 
