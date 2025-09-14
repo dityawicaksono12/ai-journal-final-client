b07c8a18e619696cccd23ecd5bfe0fe7e810f3a5
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-13 20:13:18 -0400

    Keep comments

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,25 +2,31 @@ import { createContext, useContext, useEffect, useState, } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyChats, dummyUserData } from "../assets/assets";
 
+// context obj to share state across the app
 const AppContext = createContext()
 
 export const AppContextProvider = ({ children }) => {
 
     const navigate = useNavigate()
-    const [user, setUser ] = useState(null);
-    const [chats, setChats ] = useState([]);
-    const [selectedChat, setSelectedChat ] = useState([]);
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
+    // Global states
+    const [user, setUser ] = useState(null);                                        // logged-in user
+    const [chats, setChats ] = useState([]);                                        // all available chats for the user
+    const [selectedChat, setSelectedChat ] = useState([]);                          // currently active chats
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');   // App theme (in localStorage)
+
+    // simulate fetching user data
     const fetchUser = async () => {
         setUser(dummyUserData)
     }
 
+    // simulate fetching chats for a logged-in user
     const fetchUsersChats = async () => {
         setChats(dummyChats)
         setSelectedChat(dummyChats[0])
     }
 
+    // whenever user changes, fetch/reset chats accordingly
     useEffect(() => {
         if(user){
             fetchUsersChats()
@@ -31,10 +37,12 @@ export const AppContextProvider = ({ children }) => {
         }
     },[user])
 
+    // On mount, fetch the initial user
     useEffect(() => {
         fetchUser()
     },[])
 
+    // Expose state and functions to children via context
     const value = {
         navigate, user, setUser, fetchUser, fetchUsersChats, chats, setChats, selectedChat, setSelectedChat, theme
     }
@@ -46,4 +54,5 @@ export const AppContextProvider = ({ children }) => {
     )
 }
 
+// Custom hook to consume the AppContext
 export const useAppContext = () => useContext(AppContext)
