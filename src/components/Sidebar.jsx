07c7af86cef912dd431d7d583045140653251dfd
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-14 13:59:14 -0400

    Add recent chats list with search and delete icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,6 +25,28 @@ const Sidebar = () => {
         <img src={assets.search_icon} className='w-4 not-dark:invert' alt="" />
         <input onChange={(e)=>setSearch(e.target.value)} value={search} type="text" placeholder='Search conversations' className='text-xs placeholder:text-gray-400 outline-none'/>
       </div>
+
+      {/* Recent chats (filtered by search) */}
+      {chats.length > 0 && <p className='mt-4 text-sm'>Recent Chats</p>}
+      <div className='flex-1 overflow-y-scroll mt-3 text-sm space-y-3'>
+        {
+            chats.filter((chat)=> chat.messages[0] ? chat.messages[0]?.content.toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().includes(search.toLowerCase()))
+            .map((chat)=>(
+               <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
+                    {/* Chat preview */}
+                    <div>
+                        <p className='truncate w-full'>
+                         {chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name}
+                        </p>
+                        <p className='text-xs text-gray-500 dark:text-[#B1A6C0]'>{chat.updatedAt}</p>
+                    </div>
+                    {/* Bin icon (visible on hover) to delete chat */}
+                    <img src={assets.bin_icon} className='hidden group-hover:block w-4 cursor-pointer not-dark:invert' alt=""/>
+                </div>
+            ))
+        }
+      </div>
+
     </div>
   )
 }
