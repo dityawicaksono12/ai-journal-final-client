e9a0b7abeaffccee80b3f467e877af8be42677e3
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-13 20:05:07 -0400

    Expose fetchUserChats

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,6 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState, } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyUserData } from "../assets/assets";
+import { dummyChats, dummyUserData } from "../assets/assets";
 
 const AppContext = createContext()
 
@@ -16,12 +16,27 @@ export const AppContextProvider = ({ children }) => {
         setUser(dummyUserData)
     }
 
+    const fetchUsersChats = async () => {
+        setChats(dummyChats)
+        setSelectedChat(dummyChats[0])
+    }
+
+    useEffect(() => {
+        if(user){
+            fetchUsersChats()
+        }
+        else {
+            setChats([])
+            setSelectedChat(null)
+        }
+    },[user])
+
     useEffect(() => {
         fetchUser()
     },[])
 
     const value = {
-        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChat, theme
+        navigate, user, setUser, fetchUser, fetchUsersChats, chats, setChats, selectedChat, setSelectedChat, theme
     }
 
     return (
