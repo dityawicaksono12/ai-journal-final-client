854bc3ae0a4a8d8ab939288cc1b9ec16318dcba4
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-14 02:28:45 -0400

    Add chat button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { assets } from '../assets/assets'
 const Sidebar = () => {
     // Consume global state from AppContext
     const {chats, selectedChat, theme, setTheme, user} = useAppContext()
-    
+
     // Local search input state (for future search functionality)
     const [search, setSearch] = useState('')
 
@@ -14,6 +14,11 @@ const Sidebar = () => {
     <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#2421241/30 to-[#0000001/30 border-r border-[#80609F1/30 backdrop-blur-3x1 transition-all duration-500 max-md:absolute left-0 z-1'>
       {/* Logo switches based on current theme */}
       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" className='w-full max-w-48'/>
+    
+      {/* New Chat Button */}
+      <button className='flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
+        <span className='mr-2 text-x1'>+</span> New Chat
+      </button>
     </div>
   )
 }
