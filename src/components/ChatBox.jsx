59d48803b45d9d9ae6e32f1bf585ad75faef5270
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-15 02:11:58 -0400

    add context + state to ChatBox

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useAppContext } from '../context/AppContext'
 
 const ChatBox = () => {
+  // Context: active chat + theme
+  const {selectedChat, theme} = useAppContext()
+
+  // Local state: messages + loading flag
+  const [message, setmessages] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+
   return (
     <div>
       ChatBox
