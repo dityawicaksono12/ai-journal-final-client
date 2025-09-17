baf6b4709c77ea69538717942040be7f2dd037b4
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-09-17 01:18:15 -0400

    add Credits page with loading state and dummy plans fetch

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { dummyPlans } from '../assets/assets'
+import Loading from './Loading'
 
 const Credits = () => {
+
+  const [plans, setPlans] = useState([])
+  const [loading, setLoading] = useState(true)
+  
+  // Simulated fetch (replace with real API later)
+  const fetchPlans = async () => {
+    setPlans (dummyPlans)
+    setLoading(false)
+  }
+
+  // Run fetch on mount
+  useEffect (()=>{
+    fetchPlans()
+  },[])
+    
+  if(loading) return <Loading />
+
   return (
-    <div>
-      Credits
+    <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+      <h2 className='text-3xl font-semibold text-center mb-10 xl:mt-30 text-gray-800 dark:text-white'>
+        Credit Plans
+      </h2>
     </div>
   )
 }
